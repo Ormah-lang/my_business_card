ad1a48f4a1575ba2de7d2b3bf271d6d603c66037
Chioma Onyeneke <ccchioma60@gmail.com>
2022-05-05 16:54:58 +0100

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,73 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.white30,
+        body: SafeArea(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/my_profile_pic.jpg'),
+            ),
+            Text(
+              'Chioma Onyeneke',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 25.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'SOFTWARE DEVELOPER',
+              style: TextStyle(
+                  color: Colors.teal.shade400,
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 18.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.white30,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(Icons.phone, color: Colors.teal),
+                title: Text('+447440766505',
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                        color: Colors.teal[900])),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(Icons.email, color: Colors.teal),
+                title: Text('ccchioma60@gmail.com',
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                        color: Colors.teal[900])),
+              ),
+            ),
+          ]),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
